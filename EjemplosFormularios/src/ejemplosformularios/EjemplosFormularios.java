ddd692de66f298e299569d85e16b4ba4a02f1a10
Sergio Méndez <sergioarm.gpl@gmail.com>
2020-10-27 21:24:05 -0600

    new form examples

---
+++ EjemplosFormularios/src/ejemplosformularios/EjemplosFormularios.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplosformularios;
+
+import ejemplosformularios.ejemplosbasicos.*;
+
+import ejemplosformularios.operaciones.*;
+
+/**
+ *
+ * @author sergio.mendez
+ */
+public class EjemplosFormularios {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //FrmHolaMundo f1=new FrmHolaMundo();
+        //f1.show();
+        
+        new FrmSuma().show();
+    }
+    
+}
